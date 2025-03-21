a043b5159e196cc89d282230428692456c5602a6
JoseDesen <119678328+JoseDesen@users.noreply.github.com>
2025-03-21 17:38:14 -0300

    Update servidor.js

--- servidor.js
+++ servidor.js
@@ -35,7 +35,6 @@ async function connect() {
 }
 connect();
 
-var pontuacao=0;
 app.post('/pontuacao', async (req, res) => {
   try {
     const { nome, pontuacao } = req.body;
@@ -44,7 +43,6 @@ app.post('/pontuacao', async (req, res) => {
       return res.status(400).json({ erro: 'Dados inválidos: Nome ou pontuacao ausente ou mal formatado.' });
     }else{
       const collection = client.db('sapo').collection('pontuacoes');
-      console.log('está chegando até aqui');
   
       // Obter os 5 melhores pontuacaos ordenados
       const melhorespontuacaos = await collection.find().sort({ pontuacao: 1 }).limit(20).toArray();
