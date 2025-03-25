becff073b62f74ead1c36a6c3d68db4a09f1516b
JoseDesen <119678328+JoseDesen@users.noreply.github.com>
2025-03-24 22:26:12 -0300

    Update servidor.js

--- servidor.js
+++ servidor.js
@@ -46,12 +46,19 @@ app.post('/pontuacao', async (req, res) => {
   
       // Obter os 5 melhores pontuacaos ordenados
       const melhorespontuacaos = await collection.find().sort({ pontuacao: 1 }).limit(20).toArray();
-    
+
+      console.log(melhorespontuacaos[0].pontuacao);
+      console.log(typeof melhorespontuacaos[0].pontuacao);
+      console.log(pontuacao);
+      console.log(typeof pontuacao);
+      
+      
       // Verificar se a pontuação é maior que o menor entre os melhores
       if (melhorespontuacaos.length < 20 || pontuacao < melhorespontuacaos[0].pontuacao) {
         // Adicionar a nova pontuacao
         await collection.insertOne({ nome, pontuacao });
-    
+
+        
         // Se houver mais de 20 pontuações, remover o menor
         if (melhorespontuacaos.length === 20 ) {
           const maiorpontuacao = await collection.find().sort({ pontuacao: 1 }).limit(1).toArray();
